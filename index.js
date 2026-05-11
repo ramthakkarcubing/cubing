d256a0d26e9a9cf3a785d52168ce5c1f5a8cfa99
ramthakkarcubing <ramwebsitegithub@gmail.com>
2026-05-10 19:15:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const openYT = () => {
+    window.open("https://www.youtube.com/@RamThakkar").focus();
+}
+
+const aboutRamYT = document.querySelector(".aboutRam");
+aboutRamYT.addEventListener("click", () => {
+    openYT();
+})
+
+
+
+
+
